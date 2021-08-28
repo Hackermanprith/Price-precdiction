d10749724f001ceafae2c930d3f34aba2bb62d97
Hackermanprith <82054548+Hackermanprith@users.noreply.github.com>
2021-08-28 19:18:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pandas.read_csv('iphone_price.csv')
+plt.scatter(data['version'], data['price'])
+plt.show()
